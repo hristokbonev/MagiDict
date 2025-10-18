ccd4694041a0c2a036a5fcbe5b5f9aae865d86ea
Hristo Bonev <chkbonev@gmail.com>
2025-10-18 17:06:29 +0300

    refactor: simplify setup.py by removing Windows-specific library handling

--- setup.py
+++ setup.py
@@ -1,79 +1,20 @@
-import os
+from setuptools import setup, Extension, find_packages
 import sys
 import sysconfig
-from setuptools import setup, Extension, find_packages
 
-
-extra_compile_args = []
+extra_compile_args = ["/W4"] if sys.platform == "win32" else ["-Wall", "-Wextra", "-O3"]
 extra_link_args = []
-include_dirs = [r"C:\Users\chkbo\AppData\Local\Programs\Python\Python39\include"]
-library_dirs = [r"C:\Users\chkbo\AppData\Local\Programs\Python\Python39\libs"]
-libraries = ["python39"]
-
-if sys.platform == "win32":
-    extra_compile_args = ["/W4"]
-
-    # Build Python library name (e.g., python39.lib)
-    py_version = f"python{sys.version_info.major}{sys.version_info.minor}"
-    libraries = [py_version]
-
-    # Try multiple locations for Python libs on Windows
-    python_lib_dir = None
-
-    # Method 1: Try sysconfig.get_config_var("LIBDIR")
-    libdir = sysconfig.get_config_var("LIBDIR")
-    if libdir and os.path.isdir(libdir):
-        python_lib_dir = libdir
-
-    # Method 2: Try {prefix}/libs
-    if not python_lib_dir:
-        prefix = sys.prefix
-        libs_path = os.path.join(prefix, "libs")
-        if os.path.isdir(libs_path):
-            python_lib_dir = libs_path
-
-    # Method 3: Try {exec_prefix}/libs
-    if not python_lib_dir:
-        exec_prefix = sys.exec_prefix
-        libs_path = os.path.join(exec_prefix, "libs")
-        if os.path.isdir(libs_path):
-            python_lib_dir = libs_path
-
-    if python_lib_dir:
-        library_dirs = [python_lib_dir]
-        print(f"Using Python library directory: {python_lib_dir}")
-    else:
-        print(
-            f"Warning: Could not find Python library directory. Trying default search paths."
-        )
-        print(f"sys.prefix: {sys.prefix}")
-        print(f"sys.exec_prefix: {sys.exec_prefix}")
-
-else:
-    extra_compile_args = ["-Wall", "-Wextra", "-O3"]
-    libdir = sysconfig.get_config_var("LIBDIR")
-    if libdir:
-        library_dirs = [libdir]
-
 
 magidict_ext = Extension(
     "magidict._magidict",
     sources=["magidict/_magidict.c"],
-    include_dirs=include_dirs,
-    library_dirs=library_dirs,
-    libraries=libraries,
     extra_compile_args=extra_compile_args,
     extra_link_args=extra_link_args,
     language="c",
 )
 
-
-if os.path.exists("README.md"):
-    with open("README.md", encoding="utf-8") as f:
-        long_description = f.read()
-else:
-    long_description = ""
-
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="magidict",
