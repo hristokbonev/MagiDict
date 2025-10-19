8b0e3d5ee58ff86143c8516a87e642b3cecbc91f
Hristo Bonev <chkbonev@gmail.com>
2025-10-19 19:58:10 +0300

    fix: remove unused import of typing in _magidict.pyi for cleaner code

--- magidict/_magidict.pyi
+++ magidict/_magidict.pyi
@@ -1,4 +1,3 @@
-from typing import Any, Iterable, List, Mapping, Union
 from .core import MagiDict, enchant, magi_load, magi_loads, none
 
 __all__ = ["MagiDict", "enchant", "magi_load", "magi_loads", "none"]
