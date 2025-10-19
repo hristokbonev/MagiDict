2f5944becddf5b608bbd1872f6a9fe898f6ffcb5
Hristo Bonev <chkbonev@gmail.com>
2025-10-19 18:38:15 +0300

    fix: update version to 0.1.7 in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="magidict",
-    version="0.1.4",
+    version="0.1.7",
     description="A recursive dictionary with safe attribute access and automatic conversion",
     long_description=long_description,
     long_description_content_type="text/markdown",
